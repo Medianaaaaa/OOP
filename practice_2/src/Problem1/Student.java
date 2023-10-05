fd9284dc8687c54d46e7b43c3045283ec7c2e5cf
Medianaaaaa <123486820+Medianaaaaa@users.noreply.github.com>
2023-10-05 13:29:03 +0600

    Add files via upload

---
+++ practice_2/src/Problem1/Student.java
@@ -0,0 +1,29 @@
+package Problem1;
+
+public class Student {
+	private String name;
+	private int id;
+	private int yearOfStudy;
+
+	public Student(String name, int id ) {
+		this.name = name;
+		this.id = id;
+		this.yearOfStudy = 1;
+
+	}
+
+	
+	public String getName() {
+		return name;
+	}
+	public int getId() {
+		return id;
+	}
+	public int getYear() {
+		return yearOfStudy;
+	}
+	public void incrementYear() {
+		this.yearOfStudy ++;
+	}
+	
+}
---
+++ practice_2/src/problem2/testStarTriangle.java
@@ -0,0 +1,9 @@
+package problem2;
+
+public class testStarTriangle {
+	public static void main(String[] args) {
+		StarTriangle st = new StarTriangle(5);
+		
+		System.out.println(st.toString());
+	}
+}
